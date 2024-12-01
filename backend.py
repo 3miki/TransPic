9d5c99f4eaf236a5a71ddd65cb9131b235ad36cc
Miki <90838632+3miki@users.noreply.github.com>
2024-12-01 17:55:30 +0000

    Remove unused codes

--- backend.py
+++ backend.py
@@ -19,28 +19,13 @@ def get_object_name(image: Image) -> str:
         repetition_penalty=1.2,
     )
 
-    # decode_output0 = processor.decode(raw_output[0], skip_special_tokens=True)
-    # print("decode_output0: ", decode_output0)
-    # decode_output1 = processor.decode(raw_output[0], skip_special_tokens=True)[len(prompt):]
-    # print("decode_output1: ", decode_output1)
-
     try:
-        decode_output = processor.decode(raw_output[0], skip_special_tokens=True)[len(prompt):].strip()
-        print("decode_output: ", decode_output)
+        decoded_output = processor.decode(raw_output[0], skip_special_tokens=True)[len(prompt):].strip()
+        print("decode_output: ", decoded_output)
     except Exception as e:
         print("Error: ", e)
 
-    return decode_output
-
-# vision_model_id = "google/paligemma-3b-mix-224"
-# vision_model = PaliGemmaForConditionalGeneration.from_pretrained(vision_model_id)
-# processor = AutoProcessor.from_pretrained(vision_model_id)
-# prompt = "<image><bos> What is in the image? describe the main item in the image in Japanese."
-# image_file = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg?download=true"
-# raw_image = Image.open(requests.get(image_file, stream=True).raw)
-# inputs = processor(raw_image, prompt, return_tensors="pt")
-# raw_output = vision_model.generate(**inputs, max_new_tokens=20)
-# decode_output = processor.batch_decode(raw_output, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    return decoded_output
 
 
 def translate_object_name(object_name: str, language: str) -> str:
@@ -55,28 +40,16 @@ def translate_object_name(object_name: str, language: str) -> str:
     )
 
     input_text = f"What is the following word in {language}. Respond with only the {language} translation excluding description, any special charactors. " + object_name
+
     outputs = pipe(input_text)
-    response = outputs[0]["generated_text"][len(input_text):].strip()
-    print("unprocessed response: ",outputs[0]["generated_text"])
-    print("response: ", response)
+
+    try:
+        response = outputs[0]["generated_text"][len(input_text):].strip()
+        print("unprocessed response: ",outputs[0]["generated_text"])
+        print("response: ", response)
+    except Exception as e:
+        print("Error: ", e)
     return response
-
-
-# pipe = pipeline(
-#     "text-generation",
-#     model="google/gemma-2-2b-it",
-#     device="mps",  # "mps" to run on a Mac device
-#     max_new_tokens=50, 
-#     repetition_penalty=1.2,  # higher than 1.0 is less likely to repeat text
-#     no_repeat_ngram_size=2, # prevent repeating 2-word sequences
-# )
-
-# decode_output = 'flower' # example
-# input_text = "Translate the following word into Japanese. Answer only the translation and do not include this input message. The word is: " + decode_output
-# outputs = pipe(input_text, max_new_tokens=256)
-# response = outputs[0]["generated_text"]
-# print("input: ", input_text)
-# print("response: ", response)
 
 
 # test the functions
--- dashboard.py
+++ dashboard.py
@@ -11,31 +11,28 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, message="The `use
 
 st.title('TransPic 📷')
 
+# display disclaimer
 st.info("By using this app, you accept the Google Gemma's Terms of Use found at:\nai.google.dev/gemma/terms")
 
+# login to Hugging Face
 hf_auth_token = token=os.getenv("HF_AUTH_TOKEN")
 login(hf_auth_token)
-# st.text_input("Enter your Hugging Face API token(Token will be cleared every session so please keep the copy in safe place.)", type="password")
-# if st.button("Submit", key="submit_button"):
-    # login(hf_auth_token)
-    # st.success("Successfully logged in to Hugging Face")
 
 # upload image file
-# st.write("Upload a image here:")
 uploaded_file = st.file_uploader("Choose an image file 🖼️", type=["jpg", "jpeg", "png"])
 
-# st.write("Select a language to translate:")
-language = st.selectbox("Select a language 💬", ["Japanese", "Chinese", "Spanish"])
+# select language
+language = st.selectbox("Select a language 💬", ["Japanese", "Chinese", "Spanish,"])
 
-# check if image file has been uploaded
+# show button when image is uploaded
 if uploaded_file:
+    # display image
     image = Image.open(uploaded_file)
-    # display the image
     st.image(image, caption="Uploaded Image", width=300)
 
     if st.button("Translate", key="translate_button"):    
         with st.spinner('Translating image...'):
-            # call models to generate the answer
+            # run models to generate answers
             image_name = get_object_name(image)
             st.write("Identified object name is: ", image_name)
 
